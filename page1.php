7ee709f82463f1cbbfc6d0eaba13c351b602c119
piyawan11 <98690209+piyawan11@users.noreply.github.com>
2022-02-27 18:26:02 +0700

    Create page1.php

---
+++ page1.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>การตกแต่งเว็บไซต์ด้วย CSS</title>
+<style>
+#box1{width: 800px;height: 350px;}
+.box{background-color: blue;}
+body{background-color: lightpink;}
+h1{color: yellow;      
+</style>
+</head>
+<body>
+<div id="box1" class="box">
+<h1>นางสาวปิยวรรณ เติมเสริม ม.4/12 เลขที่ 13</h1>
+</div>
+</body>
+</html>
